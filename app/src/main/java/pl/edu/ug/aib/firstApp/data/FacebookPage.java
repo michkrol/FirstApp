a08f18c90cf7fe1d19206fb1a093f984e270cfe8
Michał Król <michkrol@gmail.com>
2014-12-11 10:39:45 +0100

    Wywoływanie zadań w tle. Metody w osobnych klasach. Dodanie obsługi REST do zapytań Facebookowych.

---
+++ app/src/main/java/pl/edu/ug/aib/firstApp/data/FacebookPage.java
@@ -0,0 +1,16 @@
+package pl.edu.ug.aib.firstApp.data;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.io.Serializable;
+
+@JsonIgnoreProperties(ignoreUnknown=true)
+public class FacebookPage implements Serializable{
+    public String name;
+    public String about;
+    public String phone;
+    public String websites;
+    public int likes;
+    public String category;
+}
---
+++ app/src/main/java/pl/edu/ug/aib/firstApp/interfaces/FacebookPageRestClient.java
@@ -0,0 +1,14 @@
+package pl.edu.ug.aib.firstApp.interfaces;
+
+import org.androidannotations.annotations.rest.Get;
+import org.androidannotations.annotations.rest.Rest;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+
+import pl.edu.ug.aib.firstApp.data.FacebookPage;
+
+@Rest(rootUrl = "https://graph.facebook.com", converters = {MappingJackson2HttpMessageConverter.class})
+public interface FacebookPageRestClient {
+
+    @Get("/{name}")
+    FacebookPage getPage(CharSequence name);
+}
